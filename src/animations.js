a845ea057298e127b862cd114fd3fce3e71ea0f2
sam mok <sam@jzdoctor.com>
2019-09-24 17:37:28 +0800

    first edition for wave timelines.

--- src/animations.js
+++ src/animations.js
@@ -55,4 +55,126 @@ let transform = ({ selector, direction, target=0, delay=0, speed=200, callback, 
   return tween;
 };
 
-export { fadeIn, erase, rotate, transform };
+function initTimeline () {
+  //  balls
+  let canvas = document.querySelector('.timeline-c')
+  canvas.width = 2593
+  canvas.style.width = '2593px'
+  canvas.height = 300
+  canvas.style.height = '300px'
+  let ctx = canvas.getContext('2d')
+
+  let points = [
+    { x: 0, y: 21 },
+    { x: 77, y: 52 },
+    { x: 374, y: 0 },
+    { x: 488, y: 62 },
+    { x: 686, y: 12 },
+    { x: 898, y: 42 },
+    { x: 965, y: 8 },
+    { x: 1036, y: 39 },
+    { x: 1172, y: 12 },
+    { x: 1231, y: 99 },
+    { x: 1454, y: 45 },
+    { x: 1537, y: 88 },
+    { x: 1715, y: 41 },
+    { x: 1786, y: 70 },
+    { x: 2016, y: 22 },
+    { x: 2158, y: 119 },
+    { x: 2202, y: 59 },
+    { x: 2423, y: 123 },
+    { x: 2590, y: 76 }
+  ]
+
+  // 创建小球的构造函数
+  function Ball ({x, y}) {
+    this.x = x
+    this.y = y
+    this.originalY = y;
+    this.r = 12
+    this.color = 'rgb(255, 255, 255)'
+    this.speedY = randomNum(-3, 3) * 0.2*0.3
+  }
+  Ball.prototype = {
+    // 绘制小球
+    draw: function () {
+      // 圆边
+      ctx.beginPath()
+      ctx.globalAlpha = 1
+      ctx.fillStyle = this.color
+      ctx.lineWidth = 2
+      ctx.strokeStyle = '#436ea6'
+      ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2)
+      ctx.fill()
+      ctx.stroke()
+
+      // 圆心
+      ctx.beginPath()
+      ctx.globalAlpha = 1
+      ctx.fillStyle = '#436ea6'
+      ctx.lineWidth = 2
+      ctx.strokeStyle = '#436ea6'
+      ctx.arc(this.x, this.y, this.r / 3, 0, Math.PI * 2)
+      ctx.fill()
+      ctx.stroke()
+    },
+    // 小球移动
+    move: function () {
+      this.y += this.speedY
+      let max = 40;
+
+      if (this.y < this.originalY-max ) {
+        this.y = this.originalY-max;
+        this.speedY *= -1
+      }
+      else if (this.y > this.originalY+max) {
+        this.y = this.originalY+max;
+        this.speedY *= -1
+      }
+    }
+  }
+
+  function main () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // 小球与小球之间自动画线
+    drawLine()
+    // 使用关键帧动画，不断的绘制和清除
+    window.requestAnimationFrame(main)
+  }
+
+  function drawLine () {
+    for (let i = 0; i < balls.length; i++) {
+      for (let j = 0; j < balls.length; j++) {
+        if (i !== j) {
+          if (i + 1 === j) {
+            ctx.beginPath()
+            ctx.moveTo(balls[i].x, balls[i].y)
+            ctx.lineTo(balls[j].x, balls[j].y)
+            ctx.strokeStyle = 'black'
+            ctx.lineWidth = 2
+            ctx.globalAlpha = 0.7
+            ctx.stroke()
+          }
+        }
+      }
+
+      if (i !== 0 && i !== balls.length -1) {
+        balls[i].draw()
+        balls[i].move()
+      }
+    }
+  }
+  function randomNum (m, n) {
+    return Math.floor(Math.random() * (n - m + 1) + m)
+  }
+
+  // 存储所有的小球
+  let balls = []
+  balls = points.map(point => {
+    point.y += 100;
+    return new Ball(point);
+  })
+  main()
+}
+
+export { fadeIn, erase, rotate, transform, initTimeline };
--- src/index.js
+++ src/index.js
@@ -21,147 +21,155 @@ const ICON_DELAY = 600;
 const YEAR_DELAY = 1000;
 const PARA_DELAY = 1400;
 
-//  timelines
-let timelines = [
-  { 
-    name: '1954', speed: 'fast',
-    extraSlot2: (selector) => {
-      selector.find('.man').addClass('animated infinite leftRight');
-      selector.find('.map').addClass('animated infinite rightLeft');
-    }
-  },
-  { name: '1974' },
-  { name: '1980' },
-  { 
-    name: '1986',
-    extraSlot2: (selector) => {
-      selector.find('.icon-main').addClass(`rotate360 fast reverse`);
-    }  
-  },
-  { name: '1997' },
-  { name: '1999' },
-  { 
-    name: '2005', 
-    extraSlot2: (selector) => {
-      selector.find('.icon-main').addClass(`rotate360 fast reverse`);
-    },
-    extra: (selector) => {
-      AM.fadeIn({ selector: selector.find('.icon2'), delay: ICON_DELAY/2, callback: () => {
-        AM.rotate({ selector: selector.find('.icon2-main'), repeat: Infinity }); 
-      }});
-    } 
-  },
-  { 
-    name: '2007', 
-    speed: 10000*0.7,
-    extraSlot2: (selector) => {
-      setTimeout(() => selector.find('.icon-main').addClass(`rotate360 slow reverse`), 800);
-      selector.find('.icon-bg2').addClass(`rotate360 reverse`);
-    },
-  },
-  { 
-    name: '2009',
-    extraSlot2: (selector) => {
-      selector.find('.icon-main').addClass(`rotate360 fast reverse`);
-    }   
-  },
-  { name: '2010' },
-  { name: '2013' },
-  { 
-    name: '2015',
-    extraSlot2: (selector) => {
-      selector.find('.icon-main').addClass(`rotate360 fast reverse`);
-    }    
-  },
-  { 
-    name: '2016',
-    extraSlot2: (selector) => {
-      selector.find('.icon-main').addClass('animated infinite pulse');
-    }
-  },
-  { name: '2017' },
-  { 
-    name: '2018',
-    extraSlot2: (selector) => {
-      selector.find('.icon-bg2').addClass(`rotate360 slow reverse`);
-    } 
-  },
-  { 
-    name: '2019',
-    extraSlot2: (selector) => {
-      selector.find('.icon-main').addClass(`rotate360 fast reverse`);
-    }    
-  },
-  { 
-    name: '2019b', 
-    extraSlot2: (selector) => {
-      setTimeout(() => playEnglishVersion(), 1000);
-    }
-  }
-];
-
-//  animation play controller
-function playByTimelineIndex (index=0) {
-  let currentTimeline = timelines[index];
-  let nextTimelineIndex = index+1;
-  let isHasNext = !!timelines[nextTimelineIndex];
-
-  let selector = $(`.timeline${index+1 >= 10 ? (index+1) : '0'+(index+1)}`);
-  AM.fadeIn({ selector: selector.find('.dot01'), callback: () => selector.find('.dot01').addClass('breath') });
-  AM.erase({ 
-    selector: selector.find('.line'), 
-    delay: LINE_DELAY,
-    callback: () => {
-      AM.fadeIn({ selector: selector.find('.dot02'), delay: DOT_DELAY });
-      AM.fadeIn({ 
-        selector: selector.find('.icon'), 
-        delay: ICON_DELAY, 
-        callback: () => {
-          selector.find('.icon-bg').addClass(`rotate360 ${currentTimeline.speed || ''} ${currentTimeline.reverse ? 'reverse' : ''}`);
-          currentTimeline.extraSlot2 && currentTimeline.extraSlot2(selector);
+let app = {
+  initMainTimeline () {
+    //  timelines
+    let timelines = [
+      { 
+        name: '1954', speed: 'fast',
+        extraSlot2: (selector) => {
+          selector.find('.man').addClass('animated infinite leftRight');
+          selector.find('.map').addClass('animated infinite rightLeft');
+        }
+      },
+      { name: '1974' },
+      { name: '1980' },
+      { 
+        name: '1986',
+        extraSlot2: (selector) => {
+          selector.find('.icon-main').addClass(`rotate360 fast reverse`);
+        }  
+      },
+      { name: '1997' },
+      { name: '1999' },
+      { 
+        name: '2005', 
+        extraSlot2: (selector) => {
+          selector.find('.icon-main').addClass(`rotate360 fast reverse`);
+        },
+        extra: (selector) => {
+          AM.fadeIn({ selector: selector.find('.icon2'), delay: ICON_DELAY/2, callback: () => {
+            AM.rotate({ selector: selector.find('.icon2-main'), repeat: Infinity }); 
+          }});
         } 
+      },
+      { 
+        name: '2007', 
+        speed: 10000*0.7,
+        extraSlot2: (selector) => {
+          setTimeout(() => selector.find('.icon-main').addClass(`rotate360 slow reverse`), 800);
+          selector.find('.icon-bg2').addClass(`rotate360 reverse`);
+        },
+      },
+      { 
+        name: '2009',
+        extraSlot2: (selector) => {
+          selector.find('.icon-main').addClass(`rotate360 fast reverse`);
+        }   
+      },
+      { name: '2010' },
+      { name: '2013' },
+      { 
+        name: '2015',
+        extraSlot2: (selector) => {
+          selector.find('.icon-main').addClass(`rotate360 fast reverse`);
+        }    
+      },
+      { 
+        name: '2016',
+        extraSlot2: (selector) => {
+          selector.find('.icon-main').addClass('animated infinite pulse');
+        }
+      },
+      { name: '2017' },
+      { 
+        name: '2018',
+        extraSlot2: (selector) => {
+          selector.find('.icon-bg2').addClass(`rotate360 slow reverse`);
+        } 
+      },
+      { 
+        name: '2019',
+        extraSlot2: (selector) => {
+          selector.find('.icon-main').addClass(`rotate360 fast reverse`);
+        }    
+      },
+      { 
+        name: '2019b', 
+        extraSlot2: (selector) => {
+          setTimeout(() => playEnglishVersion(), 1000);
+        }
+      }
+    ];
+
+    //  animation play controller
+    function playByTimelineIndex (index=0) {
+      let currentTimeline = timelines[index];
+      let nextTimelineIndex = index+1;
+      let isHasNext = !!timelines[nextTimelineIndex];
+
+      let selector = $(`.timeline${index+1 >= 10 ? (index+1) : '0'+(index+1)}`);
+      AM.fadeIn({ selector: selector.find('.dot01'), callback: () => selector.find('.dot01').addClass('breath') });
+      AM.erase({ 
+        selector: selector.find('.line'), 
+        delay: LINE_DELAY,
+        callback: () => {
+          AM.fadeIn({ selector: selector.find('.dot02'), delay: DOT_DELAY });
+          AM.fadeIn({ 
+            selector: selector.find('.icon'), 
+            delay: ICON_DELAY, 
+            callback: () => {
+              selector.find('.icon-bg').addClass(`rotate360 ${currentTimeline.speed || ''} ${currentTimeline.reverse ? 'reverse' : ''}`);
+              currentTimeline.extraSlot2 && currentTimeline.extraSlot2(selector);
+            } 
+          });
+          AM.fadeIn({ selector: selector.find('.year'), delay: YEAR_DELAY });
+          AM.fadeIn({ selector: selector.find('.para'), delay: PARA_DELAY, callback: () => {
+            isHasNext && playByTimelineIndex(nextTimelineIndex);
+          } });
+          currentTimeline.extra && currentTimeline.extra(selector);
+        }
       });
-      AM.fadeIn({ selector: selector.find('.year'), delay: YEAR_DELAY });
-      AM.fadeIn({ selector: selector.find('.para'), delay: PARA_DELAY, callback: () => {
-        isHasNext && playByTimelineIndex(nextTimelineIndex);
-      } });
-      currentTimeline.extra && currentTimeline.extra(selector);
     }
-  });
-}
 
-function playEnglishVersion() {
-  let max = timelines.length;
-  function play(index) {
-    let selector = $(`.timeline${index+1 >= 10 ? (index+1) : '0'+(index+1)}`);
-    selector.find('.para').hide();
-    selector.find('.para2').addClass('active');
+    function playEnglishVersion() {
+      let max = timelines.length;
+      function play(index) {
+        let selector = $(`.timeline${index+1 >= 10 ? (index+1) : '0'+(index+1)}`);
+        selector.find('.para').hide();
+        selector.find('.para2').addClass('active');
+        setTimeout(() => {
+          selector.find('.good1').hide();
+          selector.find('.good2').addClass('active');
+        }, 1000);
+        if (index+1 === max) return setTimeout(() => location.reload(), 2000);
+        else setTimeout(() => play(index+1), 2000);
+      }
+      play(0);
+    }
+
+    window.playEnglishVersion = playEnglishVersion;
+    playByTimelineIndex(getCurrentIndex(timelines, '1954'));
+  },
+  initArrow() {
+    //  arrows
     setTimeout(() => {
-      selector.find('.good1').hide();
-      selector.find('.good2').addClass('active');
-    }, 1000);
-    if (index+1 === max) return setTimeout(() => location.reload(), 2000);
-    else setTimeout(() => play(index+1), 2000);
+      $('.arrows').addClass('active');
+      setInterval(() => {
+        $('.arrows').removeClass('active');
+        setTimeout(() => $('.arrows').addClass('active'), 250);
+      }, 4500)
+    }, 1300);
+  },
+  init () {
+    this.initMainTimeline();
+    AM.initTimeline();
   }
-  play(0);
-}
+};
 
-setTimeout(() => {
-  $('.summary, .timelines').addClass('active');
-  setTimeout(() => {
-  }, 200);
-}, 200);
+//  app init
+app.init();
 
-//  arrows
-setTimeout(() => {
-  $('.arrows').addClass('active');
-  setInterval(() => {
-    $('.arrows').removeClass('active');
-    setTimeout(() => $('.arrows').addClass('active'), 250);
-  }, 4500)
-}, 1300);
 
-window.playEnglishVersion = playEnglishVersion;
 
-playByTimelineIndex(getCurrentIndex(timelines, '1954'));
-// playByTimelineIndex(3);
+
